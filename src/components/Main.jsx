4f0041d38e76bfae1a30a3e6d2729fca0dd8a010
maksbernatskyy <maksbernatskyy@gmail.com>
2025-11-04 15:22:40 +0100

    add section content goes here

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,9 +1,19 @@
 export default function Main() {
-    return (
-        <>
-        <main>
+  return (
+    <>
+      <main>
+        <div className="bg-black py-5">
+          <div className="container">
+            <a className="text-white text-decoration-none fw-bold fs-4" href="#">
+              --&gt;Content goes here&lt;--
+            </a>
+          </div>
+        </div>
+
+        <div>
             
-        </main>
-        </>
-    )
+        </div>
+      </main>
+    </>
+  );
 }
