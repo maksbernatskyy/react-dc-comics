a2fd52b77cd9b6d4954525364d606ea26a2cdfe0
maksbernatskyy <maksbernatskyy@gmail.com>
2025-11-04 16:28:33 +0100

    add main

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,17 +1,74 @@
+import digitalComics from "../assets/img/buy-comics-digital-comics.png";
+import merchandise from "../assets/img/buy-comics-merchandise.png";
+import shopLocator from "../assets/img/buy-comics-shop-locator.png";
+import subscription from "../assets/img/buy-comics-subscriptions.png";
+import powerVisa from "../assets/img/buy-dc-power-visa.svg";
+
 export default function Main() {
   return (
     <>
       <main>
         <div className="bg-black py-5">
           <div className="container">
-            <a className="text-white text-decoration-none fw-bold fs-4" href="#">
+            <a
+              className="text-white text-decoration-none fw-bold fs-4"
+              href="#"
+            >
               --&gt;Content goes here&lt;--
             </a>
           </div>
         </div>
 
-        <div>
-            
+        <div className="bg-primary py-5">
+          <div className="container">
+            <div className="row row-cols-5">
+              <div className="col d-flex text-center">
+                <img className="me-3 d-inline" src={digitalComics} alt="" />
+                <a
+                  className="text-uppercase text-white text-decoration-none pt-2"
+                  href="#"
+                >
+                  digital comics
+                </a>
+              </div>
+              <div className="col d-flex text-center">
+                <img className="me-3" src={merchandise} alt="" />
+                <a
+                  className="text-uppercase text-white text-decoration-none pt-2"
+                  href="#"
+                >
+                  dc merchandise
+                </a>
+              </div>
+              <div className="col d-flex text-center">
+                <img className="me-3" src={subscription} alt="" />
+                <a
+                  className="text-uppercase text-white text-decoration-none pt-2"
+                  href="#"
+                >
+                  subscription
+                </a>
+              </div>
+              <div className="col d-flex text-center">
+                <img className="me-3" src={shopLocator} alt="" />
+                <a
+                  className="text-uppercase text-white text-decoration-none pt-2"
+                  href="#"
+                >
+                  comic shop locator
+                </a>
+              </div>
+              <div className="col d-flex text-center">
+                <img className="me-3" src={powerVisa} alt="" />
+                <a
+                  className="text-uppercase text-white text-decoration-none pt-2"
+                  href="#"
+                >
+                  dc power visa
+                </a>
+              </div>
+            </div>
+          </div>
         </div>
       </main>
     </>
