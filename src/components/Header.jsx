f89a7be219c89b3599d06ac4d9ee81ee55052784
maksbernatskyy <maksbernatskyy@gmail.com>
2025-11-05 12:21:11 +0100

    add dinamic navbar

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -25,60 +25,21 @@ export default function Header() {
           </div>
 
           <div>
-            <nav class="navbar navbar-expand-lg">
-              <div class="container-fluid">
-                <div class="collapse navbar-collapse" id="navbarNav">
-                  <ul class="navbar-nav">
-                    <li class="nav-item">
-                      <a class="nav-link fw-bold text-uppercase" aria-current="page" href="#">
-                        characters
-                      </a>
-                    </li>
-                    <li class="nav-item">
-                      <a class="nav-link fw-bold text-uppercase" aria-current="page" href="#">
-                        comics
-                      </a>
-                    </li>
-                    <li class="nav-item">
-                      <a class="nav-link fw-bold text-uppercase" aria-current="page" href="#">
-                        movies
-                      </a>
-                    </li>
-                    <li class="nav-item">
-                      <a class="nav-link fw-bold text-uppercase" aria-current="page" href="#">
-                        tv
-                      </a>
-                    </li>
-                    <li class="nav-item">
-                      <a class="nav-link fw-bold text-uppercase" aria-current="page" href="#">
-                        games
-                      </a>
-                    </li>
-                    <li class="nav-item">
-                      <a class="nav-link fw-bold text-uppercase" aria-current="page" href="#">
-                        collectibles
-                      </a>
-                    </li>
-                    <li class="nav-item">
-                      <a class="nav-link fw-bold text-uppercase" aria-current="page" href="#">
-                        videos
-                      </a>
-                    </li>
-                    <li class="nav-item">
-                      <a class="nav-link fw-bold text-uppercase" aria-current="page" href="#">
-                        fans
-                      </a>
-                    </li>
-                    <li class="nav-item">
-                      <a class="nav-link fw-bold text-uppercase" aria-current="page" href="#">
-                        news
-                      </a>
-                    </li>
-                    <li class="nav-item">
-                      <a class="nav-link fw-bold text-uppercase" aria-current="page" href="#">
-                        shop
-                      </a>
-                    </li>
+            <nav className="navbar navbar-expand-lg">
+              <div className="container-fluid">
+                <div className="collapse navbar-collapse" id="navbarNav">
+                  <ul className="navbar-nav">
+                    {/* Inserimento navBar dinamicamente */}
+                    {
+                      navBar.map((thisLink, i) => (
+                        <li key={i} className="nav-item">
+                          <a className="nav-link fw-bold text-uppercase" aria-current="page" href="#">
+                          {thisLink}
+                          </a>
+                        </li>
+                      ))
+                    }
+
                   </ul>
                 </div>
               </div>
