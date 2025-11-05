bbea9a7c824cb4a50b9a8207072656122483969b
maksbernatskyy <maksbernatskyy@gmail.com>
2025-11-05 13:23:55 +0100

    add cards comics

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -17,9 +17,31 @@ export default function Main() {
 
         <div className="bg-black py-5">
           <div id="currentSeriesField" className="container">
+
+            {/* Bottone current series */}
             <button id="currentSeries" className="btn btn-primary fw-bold text-uppercase">
               current series
             </button>
+
+            {/* Comics grid */}
+            <div className="row row-cols-6">
+              
+              {
+                comics.map((thisComic) => (
+                  <div key={thisComic.id} className="col">
+                    <div className="card bg-black">
+                      <img src={thisComic.thumb} className="card-img-top" alt="" />
+                      <div className="card-body">
+                        <p className="card-text text-white">{thisComic.title}</p>
+                      </div>
+                    </div>
+                  </div>
+                ))
+              }
+
+            </div>
+
+
           </div>
         </div>
 
