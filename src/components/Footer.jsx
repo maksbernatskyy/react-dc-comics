25f56d27485334b7e1e03d513aa6650e8547d392
maksbernatskyy <maksbernatskyy@gmail.com>
2025-11-05 14:35:08 +0100

    add dynamic footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,4 +1,7 @@
+// Big Logo
 import bigLogo from '../assets/img/dc-logo-bg.png'
+
+// Loghi Social
 import facebook from '../assets/img/footer-facebook.png'
 import periscope from '../assets/img/footer-periscope.png'
 import pinterest from '../assets/img/footer-pinterest.png'
@@ -6,52 +9,74 @@ import twitter from '../assets/img/footer-twitter.png'
 import youtube from '../assets/img/footer-youtube.png'
 
 export default function Footer() {
+
+    {/* function markup */}
+    function getMarkup(array) {
+        return array.map((thisElement) => (
+                                        <li className='text-decoration-none'>
+                                            <a href="#" className='text-white-50 text-decoration-none'>
+                                                {thisElement}
+                                            </a>
+                                        </li>
+                                    ))
+    }
+
+    {/* Arrays footer */}
+
+    const dcComics = [
+        'Characters',
+        'Comics',
+        'Movies',
+        'Tv',
+        'Games',
+        'Videos',
+        'News'
+    ]
+
+    const dc = [
+        'Terms Of Use',
+        'Privacy policy (New)',
+        'Ad Choices',
+        'Advertising',
+        'Jobs',
+        'Subscriptions',
+        'Talent Workshop',
+        'CPSC Certificates',
+        'Raitings',
+        'Shop Help',
+        'Contact Us'
+    ]
+
+    const sites = [
+        'DC',
+        'MAD Magazine',
+        'DC Kids',
+        'DC Universe',
+        'DC Power Visa'
+    ]
+
+    const shop = [
+        'Shop DC',
+        'Shop DC Collectibles'
+    ]
+
+
   return (
     <footer>
+
+        {/* Main Footer */}
         <div id='main-footer' className='py-5'>
             <div className="container">
                 <div id='footer-field' className="row">
-                    <div className="col-6 row">
+                    <div className="col-12 col-md-6 row">
                         <div className='col-4'>
                             <h4 className='text-uppercase text-white fw-bold'>
                                     dc comics
                             </h4>
                             <ul className='list-unstyled'>
-                                <li className='text-decoration-none'>
-                                    <a href="#" className='text-white-50 text-decoration-none'>
-                                        Characters
-                                    </a>
-                                </li>
-                                <li className='text-decoration-none'>
-                                    <a href="#" className='text-white-50 text-decoration-none'>
-                                        Comics
-                                    </a>
-                                </li>
-                                <li className='text-decoration-none'>
-                                    <a href="#" className='text-white-50 text-decoration-none'>
-                                        Movies
-                                    </a>
-                                </li>
-                                <li className='text-decoration-none'>
-                                    <a href="#" className='text-white-50 text-decoration-none'>
-                                        Tv
-                                    </a>
-                                </li>
-                                <li className='text-decoration-none'>
-                                    <a href="#" className='text-white-50 text-decoration-none'>
-                                        Games
-                                    </a>
-                                </li>
-                                <li className='text-decoration-none'>
-                                    <a href="#" className='text-white-50 text-decoration-none'>
-                                        Videos
-                                    </a>
-                                </li>
-                                <li className='text-decoration-none'>
-                                    <a href="#" className='text-white-50 text-decoration-none'>
-                                        News
-                                    </a>
-                                </li>
+                                {
+                                    getMarkup(dcComics)
+                                }
                             </ul>
 
                             <div>
@@ -59,16 +84,9 @@ export default function Footer() {
                                     shop
                                 </h4>
                                 <ul className='list-unstyled'>
-                                    <li className='text-decoration-none'>
-                                        <a href="#" className='text-white-50 text-decoration-none'>
-                                            Shop DC
-                                        </a>
-                                    </li>
-                                    <li className='text-decoration-none'>
-                                        <a href="#" className='text-white-50 text-decoration-none'>
-                                            Shop DC Collectibles
-                                        </a>
-                                    </li>
+                                    {
+                                        getMarkup(shop)
+                                    }
                                 </ul>
                             </div>
                         </div>
@@ -78,66 +96,9 @@ export default function Footer() {
                                     dc
                             </h4>
                             <ul className='list-unstyled'>
-                                <li className='text-decoration-none'>
-                                    <a href="#" className='text-white-50 text-decoration-none'>
-                                        Terms Of Use
-                                    </a>
-                                </li>
-                                <li className='text-decoration-none'>
-                                    <a href="#" className='text-white-50 text-decoration-none'>
-                                        Privacy policy (New)
-                                    </a>
-                                </li>
-                                <li className='text-decoration-none'>
-                                    <a href="#" className='text-white-50 text-decoration-none'>
-                                        Ad Choices
-                                    </a>
-                                </li>
-                                <li className='text-decoration-none'>
-                                    <a href="#" className='text-white-50 text-decoration-none'>
-                                        Advertising
-                                    </a>
-                                </li>
-                                <li className='text-decoration-none'>
-                                    <a href="#" className='text-white-50 text-decoration-none'>
-                                        Jobs
-                                    </a>
-                                </li>
-                                <li className='text-decoration-none'>
-                                    <a href="#" className='text-white-50 text-decoration-none'>
-                                        Characters
-                                    </a>
-                                </li>
-                                <li className='text-decoration-none'>
-                                    <a href="#" className='text-white-50 text-decoration-none'>
-                                        Subscriptions
-                                    </a>
-                                </li>
-                                <li className='text-decoration-none'>
-                                    <a href="#" className='text-white-50 text-decoration-none'>
-                                        Talent Workshop
-                                    </a>
-                                </li>
-                                <li className='text-decoration-none'>
-                                    <a href="#" className='text-white-50 text-decoration-none'>
-                                        CPSC Certificates
-                                    </a>
-                                </li>
-                                <li className='text-decoration-none'>
-                                    <a href="#" className='text-white-50 text-decoration-none'>
-                                        Raitings
-                                    </a>
-                                </li>
-                                <li className='text-decoration-none'>
-                                    <a href="#" className='text-white-50 text-decoration-none'>
-                                        Shop Help
-                                    </a>
-                                </li>
-                                <li className='text-decoration-none'>
-                                    <a href="#" className='text-white-50 text-decoration-none'>
-                                        Contact Us
-                                    </a>
-                                </li>
+                                {
+                                    getMarkup(dc)
+                                }
                             </ul>
                         </div>
 
@@ -146,31 +107,9 @@ export default function Footer() {
                                     sites
                             </h4>
                             <ul className='list-unstyled'>
-                                <li className='text-decoration-none'>
-                                    <a href="#" className='text-white-50 text-decoration-none'>
-                                        DC
-                                    </a>
-                                </li>
-                                <li className='text-decoration-none'>
-                                    <a href="#" className='text-white-50 text-decoration-none'>
-                                        MAD Magazine
-                                    </a>
-                                </li>
-                                <li className='text-decoration-none'>
-                                    <a href="#" className='text-white-50 text-decoration-none'>
-                                        DC Kids
-                                    </a>
-                                </li>
-                                <li className='text-decoration-none'>
-                                    <a href="#" className='text-white-50 text-decoration-none'>
-                                        DC Universe
-                                    </a>
-                                </li>
-                                <li className='text-decoration-none'>
-                                    <a href="#" className='text-white-50 text-decoration-none'>
-                                        DC Power Visa
-                                    </a>
-                                </li>
+                                {
+                                    getMarkup(sites)
+                                }
                             </ul>
                         </div>
 
@@ -183,6 +122,7 @@ export default function Footer() {
             </div>
         </div>
 
+        {/* Small Footer */}
         <div id='signup-field' className='bg-dark py-5'>
             <div className="container">
                 <div className='d-flex justify-content-between'>
